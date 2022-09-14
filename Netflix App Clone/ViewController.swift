1a28a1723362d6e069ed010b3d43499b1cfe1dec
Artem Galay <iostema@mail.ru>
2022-09-14 12:59:08 +0300

    Initial Commit

---
+++ Netflix App Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix App Clone
+//
+//  Created by Артем Галай on 14.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
